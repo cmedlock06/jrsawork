712c13f1291be0c523ec9a4c33a1f1a5929fe913
cmmedlockpaul <54674173+cmmedlockpaul@users.noreply.github.com>
2019-08-29 10:24:23 -0400

    test

---
+++ story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5fqnlR6jEai":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  myWindow = window.open("","Print","width=900,height=700,scrollbars=0,resizable=0");
+}
+
